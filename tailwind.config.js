19681170aa7034cd3d146b2c71471dd3946c1051
Amit Sarker <amitsarkerstock@gmail.com>
2023-06-09 01:09:38 +0600

    Project setup

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#DCAF27",
+          secondary: "#C27514",
+          accent: "#1C1C1C",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
